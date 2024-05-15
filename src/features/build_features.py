6a40071bc61d156d54e1b427946d589706afb65d
KuznetsovAV <kav.logrus@gmail.com>
2024-05-15 11:52:00 +0400

    add build_features script

---
+++ src/features/build_features.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def get_transform_column(df):
+    """
+    Преобразует категориальные столбцы в числовые. Обрабатывает пропущенные значения.
+    :param df:
+    :return df:
+    """
+
+    df['island'] = pd.Categorical(df['island'])
+    df['sex'] = pd.Categorical(df['sex'])
+    df = pd.get_dummies(df, columns=['island', 'sex'])
+
+    df.dropna(inplace=True)
+
+    return df
+
+
+penguins = pd.read_csv("../datasets/penguins.csv", sep=",")
+
+# Обновленный датафрейм
+update_penguins = get_transform_column(penguins)
+
+# Сохраняем наборы данных в файл CSV
+update_penguins.to_csv("../datasets/penguins.csv", index=False)
