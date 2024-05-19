f5f0b3b891053364d002251adf45208235bf89c2
user1 <teleginsky@mail.ru>
2024-05-19 19:19:08 +0300

    add test test_duplicate_values_in_penguins

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from collections import Counter
 
 # Функция для загрузки датасета
 def load_penguins_dataset():
@@ -14,3 +14,15 @@ def test_missing_values_in_penguins():
     missing_values = df.isnull().sum().sum()
 
     assert missing_values == 0, f"В датасете обнаружено {missing_values} пропущенных значений."
+
+
+def test_duplicate_values_in_penguins():
+    # Загрузка датасета
+    df = load_penguins_dataset()
+
+    # Проверка на наличие дубликатов
+    duplicates = set([tuple(x) for x in df.to_numpy()])
+    counts = Counter(duplicates)
+    dupe_counts = [(count, key) for key, count in counts.items() if count > 1]
+
+    assert len(dupe_counts) == 0, f"В датасете обнаружены дублирующиеся записи:\n{dupe_counts}"
