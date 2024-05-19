157d7ef77a6526bf356fc791d673c5842983178f
alexvk <alex@kavlab.ru>
2024-05-19 14:05:40 +0400

    add test test_missing_values_in_penguins

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,4 +1,16 @@
+import pandas as pd
 
 
-def test_zero():
-    assert True
+# Функция для загрузки датасета
+def load_penguins_dataset():
+    return pd.read_csv('datasets/penguins.csv')
+
+
+def test_missing_values_in_penguins():
+    # Загрузка датасета
+    df = load_penguins_dataset()
+
+    # Проверка на наличие пропусков
+    missing_values = df.isnull().sum().sum()
+
+    assert missing_values == 0, f"В датасете обнаружено {missing_values} пропущенных значений."
